10380fbc94751b753fe4e44d0d0c188851c73624
Marcos <marcos.chiu@gmail.com>
2024-05-27 13:04:14 -0300

    style: formatted iterator

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,32 +6,35 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 21:58:15 by maalexan          #+#    #+#             */
-/*   Updated: 2024/05/26 22:25:05 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/05/27 13:02:39 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 class Megaphone {
-    public:
-        static std::string amplify(const std::string& input) {
-            std::string output;
-            for (std::string::const_iterator it = input.begin(); it != input.end(); it++) {
-                output += std::toupper(*it);
-            }
-            return output;
-        }
+	public:
+		static std::string amplify(const std::string& input) {
+			std::string::const_iterator it = input.begin();
+			std::string::const_iterator end = input.end();
+			std::string output;
+			for (; it != end; it++) {
+				output += std::toupper(*it);
+			}
+			return output;
+		}
 };
 
 int main(int argc, char **argv) {
-    if (argc == 1) {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-        return (0);
-    }
-    std::string message;
-    for (int i = 1;i < argc; i++) {
-        message += Megaphone::amplify(argv[i]);
-        if (i < argc -1) message += ' ';
-    }
-    std::cout << message << std::endl;
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	std::string message;
+	for (int i = 1;i < argc; i++) {
+		message += Megaphone::amplify(argv[i]);
+		if (i < argc -1) message += ' ';
+	}
+	std::cout << message << std::endl;
 }
