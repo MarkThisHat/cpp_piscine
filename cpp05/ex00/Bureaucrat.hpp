3d741d504ec4967069e610d4c336bdbf50aa9ad7
Marcos <marcos.chiu@gmail.com>
2024-08-22 23:56:56 -0300

    bureaucrat hpp finished

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,29 +6,36 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 18:28:52 by maalexan          #+#    #+#             */
-/*   Updated: 2024/08/22 19:09:50 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/08/22 23:55:18 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#define HIGHEST_GRADE 1
+#define LOWEST_GRADE 150
 
 #include <string>
 
 class Bureaucrat {
  public:
   Bureaucrat();
+  Bureaucrat(const std::string name);
   Bureaucrat(const Bureaucrat& other);
   Bureaucrat& operator=(const Bureaucrat& other); 
   ~Bureaucrat();
 
   const std::string getName() const;
   const int getGrade() const;
+
   void incrementGrade(int increment);
   void decrementGrade(int decrement);
 
  private:
   const std::string name;
   int grade;
+
+  void GradeTooHighException() const;
+  void GradeTooLowException() const;
 };
 
 std::ostream& operator<<(std::ostream& ostream, const Bureaucrat& bureaucrat);
