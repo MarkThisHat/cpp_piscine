3ab006b5b3d3471b11d07b965abdf35a14d85a75
Marcos <marcos.chiu@gmail.com>
2024-08-23 23:16:01 -0300

    better display

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 11:26:12 by maalexan          #+#    #+#             */
-/*   Updated: 2024/08/23 23:10:50 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/08/23 23:14:37 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@ void endTest() {
 void createBureaucrat(const std::string name, int grade) {
   COUT(YELLOW, "Attempting to create Bureaucrat " + name + " with grade ");
   PRINT(BG_GREEN L_WHITE, grade);
+  
   try {
     Bureaucrat bureaucrat = Bureaucrat(name, grade);
-    PRINT(L_BLACK BG_YELLOW, "ok");
+    PRINT(L_BLUE, bureaucrat);
   } catch (std::exception& exception) {
     PERR(BOLD BLACK BG_RED, exception.what());
   }
+
   endTest();
 }
 
@@ -32,11 +34,13 @@ void promoteBureaucrat(Bureaucrat& bureaucrat, int increase) {
   COUT(L_BLUE, bureaucrat);
   COUT(L_GREEN, " - their rank will try to be increased by ");
   PRINT(L_GREEN, increase);
+
   try {
     bureaucrat.incrementGrade(increase);
   } catch (std::exception& exception) {
     PERR(BOLD BLACK BG_RED, exception.what());
   }
+
   PRINT(BLACK BG_GREEN, bureaucrat);
   endTest();
 }
@@ -45,11 +49,13 @@ void demoteBureaucrat(Bureaucrat& bureaucrat, int decrease) {
   COUT(L_BLUE, bureaucrat);
   COUT(L_PINK, " - their rank will try to be decreased by ");
   PRINT(L_PINK, decrease);
+
   try {
     bureaucrat.decrementGrade(decrease);
   } catch (std::exception& exception) {
     PERR(BOLD BLACK BG_RED, exception.what());
   }
+
   PRINT(BLACK BG_PINK, bureaucrat);
   endTest();
 }
