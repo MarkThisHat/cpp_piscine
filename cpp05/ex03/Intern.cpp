7f298f383b0f9a9fb0d40beebab7ffd7c0ef1c87
Marcos <marcos.chiu@gmail.com>
2024-08-27 17:08:39 -0300

    intern

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 23:21:53 by maalexan          #+#    #+#             */
-/*   Updated: 2024/08/27 00:00:24 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/08/27 17:06:29 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,49 @@
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
 
-Form* (*forms[])(std::string);
+Intern::Intern() {
+  forms[0] = { "presidential pardon", &Intern::createPresidentialPardonForm };
+  forms[1] = { "robotomy request", &Intern::createRobotomyRequestForm };
+  forms[2] = { "shrubbery creation", &Intern::createShrubberyCreationForm };
+}
 
-Intern::Intern() {}
-
-Intern::Intern(const Intern& other){}
+Intern::Intern(const Intern& other){
+  for (int i = 0; i < FORM_AMOUNT; i++) {
+    forms[i] = other.forms[i];
+  }
+}
 
 Intern& Intern::operator=(const Intern& other) {
+  if (this != &other) {
+    for (int i = 0; i < FORM_AMOUNT; i++) {
+      forms[i] = other.forms[i];
+    }
+  }
   return *this;
 }
 
 Intern::~Intern() {}
 
-Form *Intern::makeForm(const std::string formName, const std::string formTarget) {
-  return nullptr;
+Form* Intern::makeForm(const std::string formName, const std::string formTarget) {
+  for (int i = 0; i < FORM_AMOUNT; i++) {
+    if (forms[i].formName == formName)
+      return (this->*forms[i].formType)(formTarget);
+  }
+  PERR(RED BOLD, "Intern was not able to find form " + formName);
+  return NULL;
+}
+
+Form* Intern::createPresidentialPardonForm(const std::string target) {
+  PRINT(YELLOW BOLD, "Intern creates presidential pardon form");
+  return new PresidentialPardonForm(target);
+}
+
+Form* Intern::createRobotomyRequestForm(const std::string target) {
+  PRINT(CYAN BOLD, "Intern creates robotomy request form");
+  return new RobotomyRequestForm(target);
+}
+
+Form* Intern::createShrubberyCreationForm(const std::string target) {
+  PRINT(GREEN BOLD, "Intern creates shrubbery creation form");
+  return new ShrubberyCreationForm(target);
 }
--- cpp05/ex03/Intern.hpp
+++ cpp05/ex03/Intern.hpp
@@ -6,20 +6,33 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 23:18:13 by maalexan          #+#    #+#             */
-/*   Updated: 2024/08/26 23:22:38 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/08/27 17:06:44 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "AForm.hpp"
+#define FORM_AMOUNT 3
 
 class Intern {
-  public:
-   Intern();
-   Intern(const Intern& other);
-   Intern& operator=(const Intern& other);
-   ~Intern();
+ public:
+  Intern();
+  Intern(const Intern& other);
+  Intern& operator=(const Intern& other);
+  ~Intern();
 
-   Form* makeForm(const std::string formName, const std::string formTarget);
+  Form* makeForm(const std::string formName, const std::string formTarget);
+
+ private:
+  Form* createPresidentialPardonForm(const std::string target);
+  Form* createRobotomyRequestForm(const std::string target);
+  Form* createShrubberyCreationForm(const std::string target);
+
+  struct FormArchive {
+    std::string formName;
+    Form* (Intern::*formType)(const std::string);
+  };
+  
+  FormArchive forms[FORM_AMOUNT];
 };
