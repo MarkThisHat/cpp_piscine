f4e83bbf0155558c7c95e2ec4106c83e763e479e
Marcos <marcos.chiu@gmail.com>
2024-09-01 10:56:59 -0300

    int convert

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,12 +6,34 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/31 15:39:50 by maalexan          #+#    #+#             */
-/*   Updated: 2024/08/31 17:28:53 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/09/01 10:56:29 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
+int ScalarConverter::toInt(const std::string& str) {
+  errno = 0;
+  const char* convertedString = str.c_str();
+  char* endOfConverted;
+
+  long result = std::strtol(convertedString, &endOfConverted, BASE_INT);
+
+  if (convertedString == endOfConverted) {
+    throw std::invalid_argument("Input cannot be converted to int");
+  }
+  if (errno == ERANGE || result > INT_MAX || result < INT_MIN) {
+    throw std::out_of_range("Value outside boundries for an integer");
+  }
+  return static_cast<int>(result);
+}
+
 void ScalarConverter::convert(const std::string& str) {
-  std::cout << str << std::endl;
+  try {
+    int i = toInt(str);
+    std::cout << "Success! Int: " << i;
+  } catch (std::exception& e) {
+    std::cerr << "Error! Couldn't convert because: " << e.what();
+  }
+  std::cout << std::endl;
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,13 +6,24 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 16:08:56 by maalexan          #+#    #+#             */
-/*   Updated: 2024/08/31 17:29:10 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/09/01 10:55:58 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+#include <cerrno>
+#include <cstdlib>
 #include <iostream>
 #include <string>
+
+#define BASE_INT 10
+# ifndef INT_MAX
+#  define INT_MAX (2147483647)
+# endif
+
+# ifndef INT_MIN
+#  define INT_MIN (-2147483647-1)
+# endif
 
 class ScalarConverter {
  private:
@@ -20,6 +31,8 @@ class ScalarConverter {
   ~ScalarConverter();
   ScalarConverter(const ScalarConverter&);
   ScalarConverter& operator=(const ScalarConverter&);
+
+  static int toInt(const std::string& str);
 
  public:
   static void convert(const std::string& str);
