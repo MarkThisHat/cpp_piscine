14d70f24f61e482507a49f35844ed79e2de4b15e
Marcos <marcos.chiu@gmail.com>
2024-09-09 21:01:34 -0300

    trimmed spaces

--- cpp07/main.cpp
+++ cpp07/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 20:53:04 by maalexan          #+#    #+#             */
-/*   Updated: 2024/09/09 20:57:30 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/09/09 21:00:28 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@
 int main() {
   int a = 2;
   int b = 3;
-  ::swap( a, b );
+  ::swap(a, b);
   std::cout << "a = " << a << ", b = " << b << std::endl;
-  std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
-  std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+  std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+  std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
   std::string c = "chaine1";
   std::string d = "chaine2";
   ::swap(c, d);
   std::cout << "c = " << c << ", d = " << d << std::endl;
-  std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
-  std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+  std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+  std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
   return 0;
 }
