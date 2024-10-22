13fc8f6e1838cc9f3beee3ad3bb3f94af45bf25e
Marcos <marcos.chiu@gmail.com>
2024-10-22 16:53:54 -0300

    shortened

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 17:11:01 by maalexan          #+#    #+#             */
-/*   Updated: 2024/10/22 16:51:43 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/10/22 16:52:32 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,9 @@ int main(int argc, char **argv) {
   if (argc != 2) return -1;
 
   RPN rpn;
-  std::string input;
+
   try {
-    input = Parser::parseInput(argv[1]);
-    std::cout << rpn.operate(input) << std::endl;
+    std::cout << rpn.operate(Parser::parseInput(argv[1])) << std::endl;
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
