61de6de7b52dc455efcf0e83c69ccd0d609782d4
Marcos <marcos.chiu@gmail.com>
2024-10-29 15:04:03 -0300

    attempt 2

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 20:10:24 by maalexan          #+#    #+#             */
-/*   Updated: 2024/10/29 11:57:40 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/10/29 14:51:59 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,9 +47,18 @@ PmergeMe<Container, T, Allocator>::~PmergeMe() {}
 template <typename T>
 void print(std::vector<T> vec) {
   for (size_t i = 0; i < vec.size(); ++i) {
-        std::cout << vec[i] << " ";
+    if (!(i % 2)) std::cout << "<";
+    std::cout << vec[i];
+    if (i % 2 || i + 1 == vec.size() ) std::cout << ">";
+    std::cout << " ";
   }
   std::cout << std::endl;
+}
+
+template <typename T>
+void print(std::vector<T> vec, const std::string text) {
+  std::cout << text << ": ";
+  print(vec);
 }
 
 template <typename T>
@@ -59,28 +68,73 @@ void swap(T& a, T& b) {
   b = temp;
 }
 
+template <typename T>
+std::vector<T> lastRecursion(std::vector<T>& container, int size) {
+  std::vector<T> last;
+  if (size == 1) last.push_back(container[0]);
+  if (size == 2) {
+    if(container[0] > container[1]) {         
+      last.push_back(container[1]);
+      last.push_back(container[0]);
+    } else {
+      last.push_back(container[1]);
+      last.push_back(container[0]);
+    }
+  }
+  return last;
+}
+
+
+template <typename T>
+void distribute(const std::vector<T>& container, std::vector<T>& high, std::vector<T>& low) {
+  typename std::vector<T>::const_iterator iter = container.begin();
+  typename std::vector<T>::const_iterator end = container.end();
+
+  while (std::distance(iter, end) > 1) {
+    T first = *iter++;
+    T second = *iter++;
+    if (first > second) {
+      low.push_back(second);
+      high.push_back(first);
+    } else {
+      low.push_back(first);
+      high.push_back(second);
+    }
+  }
+  if (iter != end) {
+    low.push_back(*iter);
+  }
+}
+
+template <typename T>
+std::vector<T> merge(std::vector<T>& container, int size) {
+  if (size < 3) return lastRecursion(container, size);
+  int newSize = size / 2;
+
+  std::vector<T> sorted;
+  std::vector<T> unsorted;
+  sorted.reserve(newSize);
+  unsorted.reserve(size - newSize);
+  distribute(container, sorted, unsorted);
+  print(sorted, "\nhigh");
+  print(unsorted, "low");
+  merge(sorted, newSize);
+  return unsorted;
+}
+
 template <template <typename, typename> class Container, typename T, typename Allocator>
 void PmergeMe<Container, T, Allocator>::mergeInsertionSort(const Container<T, Allocator>& container, int containerSize) {
   if (containerSize < 1 || container.empty()) {
     throw std::invalid_argument("Invalid amount of elements to sort");
-  }
-
+  } 
   typename Container<T, Allocator>::const_iterator iter = container.begin();
   typename Container<T, Allocator>::const_iterator end = container.end();
 
   std::vector<T> elements;
   elements.reserve(containerSize);
-
-  while (std::distance(iter, end) > 1) {
-    T first = *iter++;
-    T second = *iter++;
-    if (first > second) swap(first, second);
-    elements.push_back(first);
-    elements.push_back(second);
-  }
-
-  if (iter != end) {
-    elements.push_back(*iter);
+  while (iter != end) {
+    elements.push_back(*iter++);
   }
   print(elements);
+  merge(elements, containerSize);
 }
