c8021b60e04f2c6f129273021612a33ac5082684
Marcos <marcos.chiu@gmail.com>
2024-10-30 11:24:57 -0300

    main

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,18 +6,26 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:14:42 by maalexan          #+#    #+#             */
-/*   Updated: 2024/10/29 20:15:28 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/10/30 11:21:06 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
-#include <list>
 #include <deque>
+#include <list>
+#include <sstream>
 
-int main() {
-  int array[] = {10, 4, 11, 6, 3, 2, 8, 5, 1, 12, 9, 13, 7};
+int* parseArguments(int argc, char* argv[]);
 
-  int size = sizeof(array) / sizeof(array[0]);
+int main(int argc, char* argv[]) {
+  int* array;
+  int size = argc - 1;
+  try {
+      array = parseArguments(argc, argv);
+  } catch (std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return -1;
+  }
 
   std::vector<int> vec(array, array + size);
   std::list<int> list(array, array + size);
@@ -28,9 +36,33 @@ int main() {
   PmergeMe<std::deque, int> dequeMerger;
   
   vecMerger.mergeInsertionSort(vec, size);
-  vecMerger.printContainer(vec);
+//  vecMerger.printContainer(vec);
   list = listMerger.mergeInsertionSort(list, size);
-  listMerger.printContainer(list);
+//  listMerger.printContainer(list);
   dequeMerger.mergeInsertionSort(deque, size);
-  dequeMerger.printContainer(deque);
+//  dequeMerger.printContainer(deque);
 }
+
+int* parseArguments(int argc, char* argv[]) {
+  if (argc < 2) throw std::invalid_argument("nothing to sort");
+
+  int* numbers = new int[argc - 1];
+  int count = 0;
+  for (int i = 1; i < argc; i++) {
+    std::istringstream iss(argv[i]);
+    int num;
+
+    if (iss >> num && iss.eof() && num >= 0) {
+      numbers[count++] = num;
+    } else {
+      delete[] numbers;
+      throw std::invalid_argument("invalid input");
+    }
+  }
+  return numbers;
+}
+
+/*
+./PmergeMe 10 4 11 6 3 2 8 5 1 12 9 13 7
+./PmergeMe `shuf -i 1-100000 -n 3000 | tr "\n" " "`
+*/
